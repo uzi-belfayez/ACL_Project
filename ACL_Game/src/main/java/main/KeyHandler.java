04709dcf0fd64b9d07a5317a33c57fe8757847f7
amina baklouti <bakloutiamina29@gmail.com>
2024-10-09 17:38:31 +0200

    Added SPRINT2 tasks

--- ACL_Game/src/main/java/main/KeyHandler.java
+++ ACL_Game/src/main/java/main/KeyHandler.java
@@ -17,16 +17,16 @@ public class KeyHandler implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 		int code = e.getKeyCode();
 		
-		if (code == KeyEvent.VK_Z) {
+		if (code == KeyEvent.VK_Z || code == KeyEvent.VK_UP){
 			upPressed  = true ;
 		}
-		if (code == KeyEvent.VK_S) {
+		if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
 			downPressed  = true ;
 		}
-		if (code == KeyEvent.VK_Q) {
+		if (code == KeyEvent.VK_Q || code == KeyEvent.VK_LEFT) {
 			leftPressed  = true ;
 			}
-		if (code == KeyEvent.VK_D) {
+		if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
 			rightPressed  = true ;
 			}
 		
@@ -35,18 +35,19 @@ public class KeyHandler implements KeyListener {
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int code = e.getKeyCode();
-		if (code == KeyEvent.VK_Z) {
+		if (code == KeyEvent.VK_Z || code == KeyEvent.VK_UP) {
 			upPressed  = false ;
 		}
-		if (code == KeyEvent.VK_S) {
+		if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
 			downPressed  = false ;
 		}
-		if (code == KeyEvent.VK_Q) {
+		if (code == KeyEvent.VK_Q || code == KeyEvent.VK_LEFT) {
 			leftPressed  = false ;
 			}
-		if (code == KeyEvent.VK_D) {
+		if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
 			rightPressed  = false ;
 			}
 	}
+
 
 }
---
+++ Documents/Sprint 2/SPRINT2.md
@@ -0,0 +1,10 @@
+# SPRINT2
+
+## Objectifs
+
+- [x]  Ajout de monstres (Emna)
+- [x]  Ajout de fantômes (Hedi)
+- [x]  Amélioration du Labyrinthe (Amina)
+- [x]  Ajout de cases spéciales dans le Labyrinthe (Rayen)
+- [x]  Ajout d'autres fonctionnalités (Tous)
+

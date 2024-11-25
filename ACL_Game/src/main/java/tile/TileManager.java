cc46c15bb558fe731253cf67a4988ed3649a9917
uzi-belfayez <rayenbelfayez@gmail.com>
2024-11-25 18:05:20 +0100

    added new map

--- ACL_Game/src/main/java/tile/TileManager.java
+++ ACL_Game/src/main/java/tile/TileManager.java
@@ -18,7 +18,7 @@ public class TileManager {
 	
 	public TileManager(GamePanel gp) {
 		this.gp = gp ;
-		tile = new Tile[10];
+		tile = new Tile[20];
 		mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 		
 		getTileImage();
@@ -28,13 +28,45 @@ public class TileManager {
 	public void getTileImage() {
 		try {
 			tile[0] = new Tile();
-			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+			tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass/grass0-dirt-mix1.png"));
+			
 			tile[1] = new Tile();
-			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall.png"));
+			tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/walls/floor_vines1.png"));
 			tile[1].collision = true ;
 			
 			tile[2] = new Tile();
-			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/cobble_blood6.png"));
+			tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass/grass1.png"));
+			
+			tile[3] = new Tile();
+			tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass/grass_flowers_yellow3.png"));
+			
+			
+			tile[4] = new Tile();
+			tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/walls/floor_vines5.png"));
+			tile[4].collision = true ;
+			
+			tile[5] = new Tile();
+			tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/walls/cobble_blood4.png"));
+			tile[5].collision = true ;
+			
+			tile[6] = new Tile();
+			tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/walls/bog_green3.png"));
+			tile[6].collision = true ;
+			
+			tile[7] = new Tile();
+			tile[7].image = ImageIO.read(getClass().getResourceAsStream("/tiles/walls/crystal_floor4.png"));
+			tile[7].collision = true ;
+			
+			tile[8] = new Tile();
+			tile[8].image = ImageIO.read(getClass().getResourceAsStream("/tiles/lava/lava3.png"));
+			
+			tile[9] = new Tile();
+			tile[9].image = ImageIO.read(getClass().getResourceAsStream("/tiles/ice/ice3.png"));
+			
+			tile[10] = new Tile();
+			tile[10].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dngn_open_sea.png"));
+			tile[10].collision = true ;
+			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
